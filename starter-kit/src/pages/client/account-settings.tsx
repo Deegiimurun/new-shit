a8d7378bea4089f4f0dde105e4fa6dfcf5bb2977
deegiimurun <deegiimurun@gmail.com>
2023-04-30 00:47:32 +0800

    fix shit

--- starter-kit/src/pages/client/account-settings.tsx
+++ starter-kit/src/pages/client/account-settings.tsx
@@ -1,162 +1,127 @@
-// ** React Imports
-import { useState, ElementType, ChangeEvent } from 'react'
-
-// ** MUI Imports
+import {useState, ElementType, ChangeEvent, FormEvent, useEffect} from 'react'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
 import Select from '@mui/material/Select'
-import Dialog from '@mui/material/Dialog'
-import { styled } from '@mui/material/styles'
-import Checkbox from '@mui/material/Checkbox'
+import {styled} from '@mui/material/styles'
 import MenuItem from '@mui/material/MenuItem'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 import InputLabel from '@mui/material/InputLabel'
-import CardHeader from '@mui/material/CardHeader'
 import FormControl from '@mui/material/FormControl'
 import CardContent from '@mui/material/CardContent'
-import DialogContent from '@mui/material/DialogContent'
-import DialogActions from '@mui/material/DialogActions'
-import FormHelperText from '@mui/material/FormHelperText'
-import InputAdornment from '@mui/material/InputAdornment'
-import Button, { ButtonProps } from '@mui/material/Button'
-import FormControlLabel from '@mui/material/FormControlLabel'
-
-// ** Third Party Imports
-import { useForm, Controller } from 'react-hook-form'
-
-// ** Icon Imports
-import Icon from 'src/@core/components/icon'
+import Button, {ButtonProps} from '@mui/material/Button'
+import {LoadingButton} from "@mui/lab";
+import {useSupabaseClient} from "@supabase/auth-helpers-react";
+import {useAuth} from "../../hooks/useAuth";
 
 interface Data {
-  email: string
-  state: string
-  address: string
-  country: string
-  lastName: string
-  currency: string
-  language: string
-  timezone: string
-  firstName: string
-  organization: string
-  number: number | string
-  zipCode: number | string
+  firstName: string,
+  lastName: string,
+  email: string,
+  age: string,
+  phoneNumber: string,
+  address: string,
+  gender: string,
+  ssn: string,
+  imageUrl: string,
+  role: string,
 }
 
 const initialData: Data = {
-  state: '',
-  number: '',
+  firstName: '',
+  lastName: '',
+  email: '',
+  age: '',
+  phoneNumber: '',
   address: '',
-  zipCode: '',
-  lastName: 'Doe',
-  currency: 'usd',
-  firstName: 'John',
-  language: 'arabic',
-  timezone: 'gmt-12',
-  country: 'australia',
-  email: 'john.doe@example.com',
-  organization: 'ThemeSelection'
+  gender: '',
+  ssn: '',
+  imageUrl: '',
+  role: '',
 }
 
-const ImgStyled = styled('img')(({ theme }) => ({
+const ImgStyled = styled('img')(({theme}) => ({
   width: 120,
   height: 120,
   borderRadius: 4,
   marginRight: theme.spacing(5)
 }))
 
-const ButtonStyled = styled(Button)<ButtonProps & { component?: ElementType; htmlFor?: string }>(({ theme }) => ({
+const ButtonStyled = styled(Button)<ButtonProps & { component?: ElementType; htmlFor?: string }>(({theme}) => ({
   [theme.breakpoints.down('sm')]: {
     width: '100%',
     textAlign: 'center'
   }
 }))
-
-const ResetButtonStyled = styled(Button)<ButtonProps>(({ theme }) => ({
-  marginLeft: theme.spacing(4),
-  [theme.breakpoints.down('sm')]: {
-    width: '100%',
-    marginLeft: 0,
-    textAlign: 'center',
-    marginTop: theme.spacing(4)
-  }
-}))
-
 const TabAccount = () => {
-  // ** State
-  const [open, setOpen] = useState<boolean>(false)
-  const [inputValue, setInputValue] = useState<string>('')
-  const [userInput, setUserInput] = useState<string>('yes')
+  const [loading, setLoading] = useState(false)
   const [formData, setFormData] = useState<Data>(initialData)
-  const [imgSrc, setImgSrc] = useState<string>('/images/avatars/1.png')
-  const [secondDialogOpen, setSecondDialogOpen] = useState<boolean>(false)
 
-  // ** Hooks
-  const {
-    control,
-    handleSubmit,
-    formState: { errors }
-  } = useForm({ defaultValues: { checkbox: false } })
+  const supabase = useSupabaseClient();
 
-  const handleClose = () => setOpen(false)
+  useEffect(() => {
+    supabase.auth.getUser().then(user => {
+      console.log(user)
+      setFormData({...initialData, ...(user.data.user?.user_metadata as Data)})
+    })
+  }, [supabase.auth])
 
-  const handleSecondDialogClose = () => setSecondDialogOpen(false)
+  const onSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    setLoading(!loading);
 
-  const onSubmit = () => setOpen(true)
+    await supabase.auth.updateUser({
+      data: formData,
+    })
 
-  const handleConfirmation = (value: string) => {
-    handleClose()
-    setUserInput(value)
-    setSecondDialogOpen(true)
+    setLoading(loading);
   }
 
-  const handleInputImageChange = (file: ChangeEvent) => {
-    const reader = new FileReader()
-    const { files } = file.target as HTMLInputElement
-    if (files && files.length !== 0) {
-      reader.onload = () => setImgSrc(reader.result as string)
-      reader.readAsDataURL(files[0])
+  const handleInputImageChange = async (file: ChangeEvent) => {
+    const {files} = file.target as HTMLInputElement
+    const fileName = String(Math.floor(Math.random() * 10000000));
 
-      if (reader.result !== null) {
-        setInputValue(reader.result as string)
-      }
-    }
-  }
-  const handleInputImageReset = () => {
-    setInputValue('')
-    setImgSrc('/images/avatars/1.png')
+    if (!files || files.length === 0) return;
+
+    await supabase.storage
+      .from("image")
+      .upload(fileName, files[0], {
+        cacheControl: "3600",
+        upsert: false,
+      });
+
+    const {data} = supabase.storage
+      .from("image")
+      .getPublicUrl(fileName);
+
+    handleFormChange('imageUrl', data.publicUrl)
   }
 
   const handleFormChange = (field: keyof Data, value: Data[keyof Data]) => {
-    setFormData({ ...formData, [field]: value })
+    setFormData({...formData, [field]: value})
   }
 
   return (
     <Grid container spacing={6}>
-      {/* Account Details Card */}
       <Grid item xs={12}>
         <Card>
-          <form>
-            <CardContent sx={{ pb: theme => `${theme.spacing(10)}` }}>
-              <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                <ImgStyled src={imgSrc} alt='Profile Pic' />
+          <form noValidate autoComplete='off' onSubmit={onSubmit}>
+            <CardContent sx={{pb: theme => `${theme.spacing(10)}`}}>
+              <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <ImgStyled src={formData.imageUrl} alt='Profile Pic'/>
                 <div>
                   <ButtonStyled component='label' variant='contained' htmlFor='account-settings-upload-image'>
                     Зураг оруулах
                     <input
                       hidden
                       type='file'
-                      value={inputValue}
                       accept='image/png, image/jpeg'
                       onChange={handleInputImageChange}
                       id='account-settings-upload-image'
                     />
                   </ButtonStyled>
-                  <ResetButtonStyled color='secondary' variant='outlined' onClick={handleInputImageReset}>
-                    Шинэчлэх
-                  </ResetButtonStyled>
-                  <Typography variant='caption' sx={{ mt: 4, display: 'block', color: 'text.disabled' }}>
+                  <Typography variant='caption' sx={{mt: 4, display: 'block', color: 'text.disabled'}}>
                     PNG JPEG өргөтгөлтэй зураг байна.
                   </Typography>
                 </div>
@@ -197,19 +162,18 @@ const TabAccount = () => {
                     fullWidth
                     label='Нас'
                     placeholder='Нас'
-                    value={formData.organization}
-                    onChange={e => handleFormChange('organization', e.target.value)}
+                    value={formData.age}
+                    onChange={e => handleFormChange('age', e.target.value)}
                   />
                 </Grid>
                 <Grid item xs={12} sm={6}>
                   <TextField
                     fullWidth
-                    type='number'
+                    type='text'
                     label='Утасны дугаар'
-                    value={formData.number}
-                    placeholder='Утасныдугаар'
-                    onChange={e => handleFormChange('number', e.target.value)}
-                    InputProps={{ startAdornment: <InputAdornment position='start'>US (+1)</InputAdornment> }}
+                    value={formData.phoneNumber}
+                    placeholder='Утасны дугаар'
+                    onChange={e => handleFormChange('phoneNumber', e.target.value)}
                   />
                 </Grid>
                 <Grid item xs={12} sm={6}>
@@ -226,8 +190,8 @@ const TabAccount = () => {
                     <InputLabel>Хүйс</InputLabel>
                     <Select
                       label='Хүйс'
-                      value={formData.country}
-                      onChange={e => handleFormChange('country', e.target.value)}
+                      value={formData.gender}
+                      onChange={e => handleFormChange('gender', e.target.value)}
                     >
                       <MenuItem value='male'>Эр</MenuItem>
                       <MenuItem value='female'>Эм</MenuItem>
@@ -239,17 +203,14 @@ const TabAccount = () => {
                     fullWidth
                     label='Регистрийн дугаар'
                     placeholder='Регистрийн дугаар'
-                    value={formData.address}
-                    onChange={e => handleFormChange('address', e.target.value)}
+                    value={formData.ssn}
+                    onChange={e => handleFormChange('ssn', e.target.value)}
                   />
                 </Grid>
                 <Grid item xs={12}>
-                  <Button variant='contained' sx={{ mr: 4 }}>
+                  <LoadingButton loading={loading} size='large' type='submit' variant='contained' sx={{mr: 4}}>
                     Хадгалах
-                  </Button>
-                  <Button type='reset' variant='outlined' color='secondary' onClick={() => setFormData(initialData)}>
-                    Шинэчлэх
-                  </Button>
+                  </LoadingButton>
                 </Grid>
               </Grid>
             </CardContent>
