7642a15e5c65fe141181b6772b1494e84ba6f775
Tunamalchimeg <78781397+Tunamalchimeg@users.noreply.github.com>
2023-05-01 01:12:53 +0800

    nice shit

--- starter-kit/src/pages/client/index.tsx
+++ starter-kit/src/pages/client/index.tsx
@@ -3,5 +3,5 @@ import {useRouter} from "next/router";
 export default function Client() {
   const router = useRouter();
 
-  router.push('/client/home')
+  router.push('/client/appointment')
 }
--- starter-kit/src/pages/doctor/index.tsx
+++ starter-kit/src/pages/doctor/index.tsx
@@ -1,7 +1,7 @@
-import {useRouter} from "next/router";
+import { useRouter } from 'next/router'
 
 export default function Doctor() {
-  const router = useRouter();
+  const router = useRouter()
 
-  router.push('/doctor/home')
+  router.push('/doctor/all_patient/index.tsx')
 }
